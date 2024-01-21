00caf420c5945d52cf54cf38743f232ef9ee214b
YasTheGoat <ayassinee2007@gmail.com>
2024-01-20 20:30:32 -0500

    fix : the colors now also work with tailwind

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -2,7 +2,21 @@ return {
 	{
 		"NvChad/nvim-colorizer.lua",
 		config = function()
-			require("colorizer").setup()
+			require("colorizer").setup({
+				filetypes = { "*" },
+				user_default_options = {
+					RRGGBBAA = true, -- #RRGGBBAA hex codes
+					AARRGGBB = true, -- 0xAARRGGBB hex codes
+					css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+					css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+					mode = "background", -- Set the display mode.
+					tailwind = true, -- Enable tailwind colors
+					sass = { enable = false, parsers = { "css" } }, -- Enable sass colors
+					virtualtext = "■",
+					always_update = true,
+				},
+				buftypes = {},
+			})
 		end,
 	},
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,7 +4,6 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set clipboard=unnamed")
 
 vim.g.mapleader = " "
 
