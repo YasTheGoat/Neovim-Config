7ec32ef887d248d0782a97dc233a2f9c5339f56d
YasTheGoat <ayassinee2007@gmail.com>
2024-01-21 19:50:25 -0500

    some changes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,49 +1,69 @@
 local lsps = {
-  "lua_ls",
-  "clangd",
-  "gopls",
-  "tsserver",
-  "pyright",
-  "templ",
-  "html",
-  "htmx",
-  "tailwindcss",
+
+	"clang-format",
+	"black",
+	"isort",
+	"pyright",
+	"prettier",
+
+	"eslint_d",
+	"checkmake",
 }
 
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = lsps,
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"clangd",
+					"gopls",
+					"tsserver",
+					"pyright",
+					"templ",
+					"html",
+					"htmx",
+					"tailwindcss",
+					"cmake",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.clangd.setup({ capabilities = capabilities })
-      lspconfig.gopls.setup({ capabilities = capabilities })
-      lspconfig.tsserver.setup({ capabilities = capabilities })
-      lspconfig.pyright.setup({ capabilities = capabilities })
-      lspconfig.templ.setup({ capabilities = capabilities })
-      lspconfig.html.setup({ capabilities = capabilities, filetypes = { "html", "templ" } })
-      lspconfig.htmx.setup({ capabilities = capabilities, filetypes = { "html", "templ" } })
-      lspconfig.tailwindcss.setup({
-        capabilities = capabilities,
-        filetypes = { "templ", "html", "javascript", "javascriptreact", "javascript.jsx" ,"typescript", "typescriptreact", "typescript.tsx" },
-        init_options = { userLanguages = { templ = "html" } },
-      })
-    end,
-  },
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.clangd.setup({ capabilities = capabilities })
+			lspconfig.gopls.setup({ capabilities = capabilities })
+			lspconfig.tsserver.setup({ capabilities = capabilities })
+			lspconfig.pyright.setup({ capabilities = capabilities })
+			lspconfig.templ.setup({ capabilities = capabilities })
+			lspconfig.html.setup({ capabilities = capabilities, filetypes = { "html", "templ" } })
+			lspconfig.htmx.setup({ capabilities = capabilities, filetypes = { "html", "templ" } })
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+				filetypes = {
+					"templ",
+					"html",
+					"javascript",
+					"javascriptreact",
+					"javascript.jsx",
+					"typescript",
+					"typescriptreact",
+					"typescript.tsx",
+				},
+				init_options = { userLanguages = { templ = "html" } },
+			})
+		end,
+	},
 }
--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -5,6 +5,7 @@ return {
 		config = function()
 			local config = require("nvim-treesitter.configs")
 			config.setup({
+				ensure_installed = { "lua" },
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },
