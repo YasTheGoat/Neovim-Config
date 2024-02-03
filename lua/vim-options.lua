2857c19999b8ee5592877e2f2ffc34ea34bc9e80
YasTheGoat <ayassinee2007@gmail.com>
2024-02-02 19:45:35 -0500

    The cursor is different in insert mode vs normal mode

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,4 +1,3 @@
-vim.cmd("set guicursor=n-v-c-i:block")
 vim.cmd("set number")
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
@@ -21,15 +20,15 @@ vim.keymap.set("n", "<leader>P", '"+P', default_opts)
 vim.filetype.add({ extension = { templ = "templ" } })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(ev)
-    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    local opts = { buffer = ev.buf }
+		local opts = { buffer = ev.buf }
 
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-  end,
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+	end,
 })
