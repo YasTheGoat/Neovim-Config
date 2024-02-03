2521280e62f2b081d16af140b3d72ed120d2d320
YasTheGoat <ayassinee2007@gmail.com>
2024-02-02 19:49:31 -0500

    added highlight to special comments such as TODO. FIX. etc...

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,9 +1,18 @@
 return {
-  {
-    "numToStr/Comment.nvim",
-    lazy = false,
-    config = function()
-      require("Comment").setup()
-    end,
-  },
+	{
+		"numToStr/Comment.nvim",
+		lazy = false,
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 }
