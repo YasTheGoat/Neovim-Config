83df981a6999d0b40661495000d19c7578c877e6
YasTheGoat <ayassinee2007@gmail.com>
2024-02-24 18:28:20 -0500

    added code folding with treesitter and trouble plugin

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -55,6 +55,20 @@ return {
 				init_options = { userLanguages = { templ = "html" } },
 			})
 			lspconfig.prismals.setup({ capabilities = capabilities })
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+					local opts = { buffer = ev.buf }
+
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+				end,
+			})
 		end,
 	},
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,3 +1,7 @@
+vim.cmd("set foldlevel=20")
+vim.cmd("set foldmethod=expr")
+vim.cmd("set foldexpr=nvim_treesitter#foldexpr()")
+
 vim.cmd("set number")
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
@@ -19,16 +23,3 @@ vim.keymap.set("n", "<leader>P", '"+P', default_opts)
 
 vim.filetype.add({ extension = { templ = "templ" } })
 
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(ev)
-		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-		local opts = { buffer = ev.buf }
-
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-	end,
-})
