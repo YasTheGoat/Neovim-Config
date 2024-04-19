f0d59bb41efa20024d1eb673a0793e776d1210de
YasTheGoat <ayassinee2007@gmail.com>
2024-04-18 20:46:10 -0400

    fea : changed catppuccin theme to gruvbox

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -26,4 +26,8 @@ vim.keymap.set("n", "<leader>p", '"+p', default_opts)
 vim.keymap.set("n", "<leader>P", '"+P', default_opts)
 
 vim.filetype.add({ extension = { templ = "templ" } })
-
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false
+    }
+)
